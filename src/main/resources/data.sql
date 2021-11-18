29b4aa7c1bd279d7fe8dbc0505a277a4adbd6971
dydko1 <miroslaw.dyduch@gmail.com>
2021-11-18 20:17:17 +0100

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO country1 (name) VALUES ('India');
+INSERT INTO country1 (name) VALUES ('Brazil');
+INSERT INTO country1 (name) VALUES ('USA');
+INSERT INTO country1 (name) VALUES ('Italy');
+INSERT INTO country1 (name) VALUES ('Poland');
