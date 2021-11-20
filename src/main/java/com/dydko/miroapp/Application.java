03ff67c9e83cfa3af11fb9d9fc33b9d0aff85b6a
start.vaadin.com <no-reply@vaadin.com>
2021-11-20 20:21:48 +0000

    Generated project

---
+++ src/main/java/com/dydko/miroapp/Application.java
@@ -0,0 +1,30 @@
+package com.dydko.miroapp;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "practicalbookspringboot")
+@PWA(name = "Practical Book Spring Boot", shortName = "Practical Book Spring Boot", offlineResources = {
+        "images/logo.png"})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
