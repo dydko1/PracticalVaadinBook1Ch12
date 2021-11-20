91c5fb16ff526a638ba9bca5aa2b35649db5fe68
dydko1 <miroslaw.dyduch@gmail.com>
2021-11-20 22:00:11 +0100

    temp

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users(email, user_password, birth_date, favorite_number) VALUES
+  ('miro1@wp.pl','secret_pass1','1990-03-11',111),
+  ('miro2@wp.pl','secret_pass2','1991-03-12',222),
+  ('miro3@wp.pl','secret_pass3','1992-03-13',333);
+
+INSERT INTO country1 (name) VALUES ('India');
+INSERT INTO country1 (name) VALUES ('Brazil');
+INSERT INTO country1 (name) VALUES ('USA');
+INSERT INTO country1 (name) VALUES ('Italy');
+INSERT INTO country1 (name) VALUES ('Poland');
