6c4c73ba64a9ed32b9027c8dc7c7c9e066fba845
dydko1 <miroslaw.dyduch@gmail.com>
2021-11-27 08:26:55 +0100

    added component form - temp

--- src/main/java/com/dydko/application/views/list/ListView.java
+++ src/main/java/com/dydko/application/views/list/ListView.java
@@ -1,9 +1,13 @@
 package com.dydko.application.views.list;
 
 import com.dydko.application.views.data.entity.Contact;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -18,12 +22,28 @@ public class ListView extends VerticalLayout {
         addClassName("miro-list");
         setSizeFull();
         configureGrid();
-        add(grid);
+        add(getToolbar(), grid);
     }
 
     private void configureGrid() {
-        grid.addClassName("contact-grid");
+        grid.addClassNames("contact-grid");
         grid.setSizeFull();
-        grid.setColumns("firstName","lastName","email");
+        grid.setColumns("firstName", "lastName", "email");
+        grid.addColumn(cont -> cont.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(column->column.setAutoWidth(true));
+    }
+
+    private HorizontalLayout getToolbar() {
+        HorizontalLayout toolbar = new HorizontalLayout();
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button addContactButton = new Button("Add contact");
+        toolbar.add(filterText, addContactButton);
+        toolbar.addClassName("toolBar");
+        toolbar.setId("Miro1");
+        return toolbar;
     }
 }
